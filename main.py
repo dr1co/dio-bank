0bf02b9d8beb0f0b5ee91b5b069a2e9e749620d8
dr1co <dricomatoba@hotmail.com>
2023-09-19 17:16:50 -0300

    [feat]: added user and acc creation

--- main.py
+++ main.py
@@ -1,4 +1,46 @@
-def saqueDinheiro(saldo, valor, saques_dia):
+import re
+
+def valida_data(data):
+    pattern = re.compile(r"\d\d/\d\d/\d\d\d\d", re.IGNORECASE)
+    return pattern.match(data)
+
+def criar_usuario(cpf):
+  nome = input("  Por favor, informe-nos seu nome: ")
+  while True:
+    data_nascimento = input("  Agora, sua data de nascimento, no formato DD/MM/AAAA: ")
+    if not valida_data(data_nascimento):
+      print("  Por favor, informe-nos uma data válida")
+    else: break
+  logradouro = input("  Agora, o seu logradouro: ")
+  while True:
+    numero = input("  O número da residência: ")
+    if not numero.isdigit():
+      print("  Por favor, forneça um número válido!")
+    else: break
+  bairro = input("  O nome do bairro da residência: ")
+  cidade = input("  O nome da cidade: ")
+  while True:
+    estado = input("  A sigla do estado:")
+    if len(estado) != 2:
+      print("Por favor, forneça uma sigla válida")
+    else: break
+  
+  novo_usuario = {
+    "nome": nome,
+    "data_nascimento": data_nascimento,
+    "endereco": f'{logradouro}, {numero} - {bairro} - {cidade}/{estado}',
+  }
+  usuarios[cpf] = novo_usuario
+
+  print("  Usuário criado com sucesso!")
+
+def criar_conta_corrente(agencia, num_conta, cpf_usuario):
+  pass
+
+def formatar_cpf(cpf):
+  return f'{cpf[0:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9::]}'
+
+def saque_dinheiro(saldo, valor, saques_dia):
   if valor <= 0:
     print("  Não foi possível realizar o saque: valor inválido!")
   elif saques_dia >= LIMITE_NUM_SAQUES:
@@ -15,7 +57,7 @@ def saqueDinheiro(saldo, valor, saques_dia):
   
   return (saldo, saques_dia)
 
-def depositoDinheiro(saldo, valor):
+def deposito_dinheiro(saldo, valor):
   if valor <= 0:
     print("  Não foi possível realizar o depósito: valor inválido!")
   else:
@@ -25,59 +67,89 @@ def depositoDinheiro(saldo, valor):
   
   return saldo
 
-def visualizarExtrato(saldo):
+def visualizar_extrato(saldo, extrato):
   print(f"  Seu saldo bancário é de R${format(saldo, '.2f')}")
 
-p_saldo = 1000.00
-p_saques_dia = 0
+usuarios = {
+  "000.000.000-00": {
+    "nome": "Teste da Silva Sauro",
+    "data_nascimento": "31/02/1500",
+    "endereco": "Rua Jefferson, 115 - Libélula - Araçapuca da Terra/MS",
+  },
+}
+contas = {
+  1: {
+    "saldo": 1000.00,
+    "saques_dia": 0,
+    "extrato": {},
+    "detentor": "000.000.000-00"
+  },
+}
 LIMITE_NUM_SAQUES = 3
 LIMITE_VALOR_SAQUE = 500
+AGENCIA = "0001"
 
+print(f'''
+  {"DIO-BANK".center(40, "-")}
+  Por favor, identifique-se!
+''')
 
 menu = f'''
   {"DIO-BANK".center(40, "-")}
-  Bem vindo ao Dio-Bank! O que deseja fazer?
+  Bem vindo ao Dio-Bank, {nome}! O que deseja fazer?
 
   [1] => Depósito
   [2] => Saque
   [3] => Ver extrato
+  [4] => Criar nova conta-corrente
   [0] => Sair
 '''
+
+while True:
+  cpf = input("  Seu CPF: ")
+  if not cpf.isdigit() and len(cpf) != 11:
+    print("  CPF inválido! Por favor digite novamente!")
+  else:
+    cpf = formatar_cpf(cpf)
+    break
+
+if usuarios.index(cpf) == -1:
+  print("  Parece que o seu CPF não está cadastrado no sistema, gostaria de se cadastrar como novo usuário?")
+  resp = input("  [S/N]: ")
+  if resp == 'S':
+    usuario = criar_usuario(cpf)
 
 print(menu)
 
 while True:
-  opcao = -1
-  
-  while opcao != '0' and opcao != '1' and opcao != '2' and opcao != '3':
+  while True:
     opcao = input("  Sua opção: ")
-    if opcao != '0' and opcao != '1' and opcao != '2' and opcao != '3':
+    if opcao != '0' and opcao != '1' and opcao != '2' and opcao != '3' and opcao != '4':
       print("  Digite uma opção válida!")
+    else: break
 
   opcao = int(opcao)
 
   if opcao == 1:
-    valor_deposito = "a"
-
-    while not valor_deposito.isdigit():
+    while True:
       valor_deposito = input("  Digite qual valor deseja depositar: ")
-      if (not valor_deposito.isdigit()):
+      if not valor_deposito.isdigit():
         print("  Digite um número válido!")
+      else: break
     
-    p_saldo = depositoDinheiro(p_saldo, int(valor_deposito))
+    saldo = deposito_dinheiro(saldo, int(valor_deposito))
 
   elif opcao == 2:
-    valor_saque = "a"
-    
-    while not valor_saque.isdigit():
+    while True:
       valor_saque = input("  Digite qual valor deseja sacar: ")
-      if (not valor_saque.isdigit()):
+      if not valor_saque.isdigit():
         print("  Digite um número válido!")
+      else: break
 
-    (p_saldo, p_saques_dia) = saqueDinheiro(p_saldo, int(valor_saque), p_saques_dia)
+    (saldo, saques_dia) = saque_dinheiro(saldo=saldo, valor_saque=int(valor_saque), saques_dia=saques_dia)
 
   elif opcao == 3:
-    visualizarExtrato(p_saldo)
+    visualizar_extrato(saldo, extrato=extrato)
 
   elif opcao == 0:
     print("  Obrigado por usar o Dio-Bank, ficamos felizes em tê-lo como cliente!")
